2a639acacb0f9c221f1f9b432038279c5cd7d875
Lucas Kemmer <lucas@n3-100-154.dhcp.drexel.edu>
2024-02-05 13:57:02 -0500

    Basic button elements

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableHighlight } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>I set up the environment</Text>
-      <StatusBar style="auto" />
+      <View style={styles.blackBackground}>
+        <View style={styles.buttonContainer}>
+          <TouchableHighlight
+            style={styles.squareButton}
+            onPress={() => console.log("Button 1 pressed")}
+          >
+            <Text style={styles.buttonText}>1</Text>
+          </TouchableHighlight>
+
+          <TouchableHighlight
+            style={styles.squareButton}
+            onPress={() => console.log("Button 2 pressed")}
+          >
+            <Text style={styles.buttonText}>2</Text>
+          </TouchableHighlight>
+
+          <TouchableHighlight
+            style={styles.squareButton}
+            onPress={() => console.log("Button 3 pressed")}
+          >
+            <Text style={styles.buttonText}>3</Text>
+          </TouchableHighlight>
+
+          <TouchableHighlight
+            style={styles.squareButton}
+            onPress={() => console.log("Button 4 pressed")}
+          >
+            <Text style={styles.buttonText}>4</Text>
+          </TouchableHighlight>
+
+          <TouchableHighlight
+            style={styles.squareButton}
+            onPress={() => console.log("Button 5 pressed")}
+          >
+            <Text style={styles.buttonText}>5</Text>
+          </TouchableHighlight>
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +49,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "lightblue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  blackBackground: {
+    flex: 1,
+    backgroundColor: "black",
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    position: "absolute",
+    bottom: "2%",
+    width: "100%",
+  },
+  squareButton: {
+    width: "20%",
+    aspectRatio: 1,
+    borderRadius: 10,
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "black",
+    fontSize: 16,
   },
 });
