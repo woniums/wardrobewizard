4c409d537bd76caad7e2295cadcd8bb7602fdf3e
Lucas Kemmer <lucas@n3-100-154.dhcp.drexel.edu>
2024-02-08 14:52:33 -0500

    Testing the Profile Page

--- navigation/screens/ProfileScreen.js
+++ navigation/screens/ProfileScreen.js
@@ -4,7 +4,7 @@ import { View, Text } from "react-native";
 export default function ProfileScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text style={{ fontSize: 26, fontWeight: "bold" }}>Profile Screen</Text>
+      <Text style={{ fontSize: 26, fontWeight: "bold" }}> Screen</Text>
     </View>
   );
 }
