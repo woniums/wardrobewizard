6635f2098c3b9bd493d08573fb2e693f85c0bf1f
danielbisma05@gmail.com <danielbisma05@gmail.com>
2024-02-16 14:16:09 -0500

    Initial Commit

--- navigation/screens/ProfileScreen.js
+++ navigation/screens/ProfileScreen.js
@@ -3,8 +3,11 @@ import { View, Text } from "react-native";
 
 export default function ProfileScreen({ navigation }) {
   return (
+
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text style={{ fontSize: 26, fontWeight: "bold" }}> Screen</Text>
+
+      <img></img>
+      <Text style={{ fontSize: 26, fontWeight: "bold" }}> Profile Screen</Text>
     </View>
   );
 }
