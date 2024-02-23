41e7e6a86d2cabc0e74ad59e60f87833da7bc80d
Lucas Kemmer <lucas@n3-100-154.dhcp.drexel.edu>
2024-02-23 14:58:50 -0500

    Fixed Navagation for renamed page

--- navigation/MainContainer.js
+++ navigation/MainContainer.js
@@ -5,7 +5,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 // Screens
 import FeedScreen from "./screens/FeedScreen";
 import CameraScreen from "./screens/CameraScreen";
-import AlbumsScreens from "./screens/AlbumsScreen";
+import AlbumsScreen from "./screens/AlbumsScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 
 const FeedName = "Feed";
@@ -22,7 +22,7 @@ function MainContainer() {
       <Tab.Navigator screenOptions={{ headerShown: false }}>
         <Tab.Screen name={FeedName} component={FeedScreen} />
         <Tab.Screen name={CameraName} component={CameraScreen} />
-        <Tab.Screen name={AlbumsName} component={OutfitsScreen} />
+        <Tab.Screen name={AlbumsName} component={AlbumsScreen} />
         <Tab.Screen name={ProfileName} component={ProfileScreen} />
       </Tab.Navigator>
     </NavigationContainer>
