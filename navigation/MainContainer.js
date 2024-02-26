9845c74169814c36a16d571b77d50351f4515dc8
Lucas Kemmer <lucas@n3-100-154.dhcp.drexel.edu>
2024-02-26 11:26:56 -0500

    Fixed Navigation to renamed page

--- navigation/MainContainer.js
+++ navigation/MainContainer.js
@@ -5,13 +5,13 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 // Screens
 import FeedScreen from "./screens/FeedScreen";
 import CameraScreen from "./screens/CameraScreen";
-import OutfitsScreen from "./screens/OutfitsScreen";
+import CreateScreen from "./screens/CreateScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 
 const FeedName = "Feed";
 const CameraName = "Camera";
 const ProfileName = "Profile";
-const OutfitsName = "Outfit";
+const CreateName = "Create";
 
 //creates the bottom navigation bar on each of the screens
 const Tab = createBottomTabNavigator();
@@ -22,7 +22,7 @@ function MainContainer() {
       <Tab.Navigator screenOptions={{ headerShown: false }}>
         <Tab.Screen name={FeedName} component={FeedScreen} />
         <Tab.Screen name={CameraName} component={CameraScreen} />
-        <Tab.Screen name={OutfitsName} component={OutfitsScreen} />
+        <Tab.Screen name={CreateName} component={CreateScreen} />
         <Tab.Screen name={ProfileName} component={ProfileScreen} />
       </Tab.Navigator>
     </NavigationContainer>
