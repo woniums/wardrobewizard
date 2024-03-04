641458cc0d0f3591062773d302f0fb1fc9dfafe7
Lucas Kemmer <lucas@n3-100-154.dhcp.drexel.edu>
2024-03-03 20:49:11 -0500

    Adding a Drop-Down menu for tagging clothing

--- navigation/screens/CreateScreen.js
+++ navigation/screens/CreateScreen.js
@@ -3,64 +3,110 @@ import {
   StyleSheet,
   View,
   SafeAreaView,
-  TextInput,
   TouchableOpacity,
   Text,
   Image,
+  useWindowDimensions,
 } from "react-native";
 import { useRoute } from "@react-navigation/native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { SelectList } from "react-native-dropdown-select-list";
 
-const TextInputButton = () => {
-  const [number, onChangeNumber] = useState("");
+const TextInputButton = ({ onTag, uri }) => {
+  const [selectedValue, setSelectedValue] = useState("");
+  const handleTag = () => {
+    if (selectedValue) {
+      onTag({ uri, tag: selectedValue });
+      setSelectedValue("");
+    }
+  };
 
   return (
     <SafeAreaView>
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeNumber}
-        value={number}
-        placeholder="Enter something..."
-        keyboardType="default"
+      <SelectList
+        setSelected={(val) => setSelectedValue(val)}
+        data={[
+          { key: "1", value: "Top" },
+          { key: "2", value: "Bottom" },
+          { key: "3", value: "Shoes" },
+          { key: "4", value: "Accessory" },
+        ]}
+        save="value"
       />
+      <TouchableOpacity onPress={handleTag} style={styles.button}>
+        <Text>Tag Image</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
 
 const Tag = () => {
-  const [showTextInput, setShowTextInput] = useState(false);
-  const handleClick = () => {
-    setShowTextInput(true);
-  };
+  const [currIndex, setCurrIndex] = useState(0);
+  const [tags, setTags] = useState({});
   const route = useRoute();
   const images = route.params?.images || [];
+  const windowHeight = useWindowDimensions().height;
+  const windowWidth = useWindowDimensions().width;
+
+  const handleTag = ({ uri, tag }) => {
+    setTags((prevTags) => ({
+      ...prevTags,
+      [uri]: tag,
+    }));
+  };
+
+  const getNextImage = () => {
+    if (currIndex < images.length - 1) {
+      setCurrIndex(currIndex + 1);
+    }
+  };
+  const getPrevImage = () => {
+    if (currIndex > 0) {
+      setCurrIndex(currIndex - 1);
+    }
+  };
 
   return (
-    <View>
-      {showTextInput && <TextInputButton />}
-      <TouchableOpacity onPress={handleClick} style={styles.button}>
-        <Text>Click me</Text>
-      </TouchableOpacity>
-      <View>
-        {images.length === 0 ? (
-          <Text>No Images to display</Text>
-        ) : (
-          images.map((image, index) => (
-            <Image
-              key={index}
-              source={{ uri: image }}
-              style={{ width: 200, height: 200, marginBottom: 10 }}
-            />
-          ))
-        )}
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          {images.length === 0 ? (
+            <Text>No Images to display</Text>
+          ) : (
+            <>
+              <TouchableOpacity onPress={getPrevImage} style={{ paddingHorizontal: 5 }}>
+                <Icon name="arrow-left" size={30} color="black" />
+              </TouchableOpacity>
+              <Image
+                source={{ uri: images[currIndex] }}
+                style={{
+                  width: Math.min(windowWidth * 0.8, windowHeight * 0.8),
+                  height: Math.min(windowWidth * 0.8, windowHeight * 0.8),
+                  aspectRatio: 1,
+                }}
+              />
+              <TouchableOpacity onPress={getNextImage} style={{ paddingHorizontal: 5 }}>
+                <Icon name="arrow-right" size={30} color="black" />
+              </TouchableOpacity>
+            </>
+          )}
+        </View>
+        <TextInputButton onTag={handleTag} uri={images[currIndex]} />
+        {console.log(tags)}
       </View>
-    </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   input: {
     height: 40,
-    margin: 12,
     borderWidth: 1,
     padding: 10,
   },
