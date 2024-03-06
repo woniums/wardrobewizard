e9f1921185f5c5198f027bb158ab2216e1fc6c69
Lucas Kemmer <lucas@n3-100-154.dhcp.drexel.edu>
2024-03-06 13:19:59 -0500

    Clicked Albums now show contents

--- navigation/screens/AlbumsScreen.js
+++ navigation/screens/AlbumsScreen.js
@@ -3,24 +3,62 @@ import { useState } from "react";
 import {
   View,
   FlatList,
-  StyleSheet,
+  Dimensions,
   Text,
   TouchableOpacity,
   Image,
   SafeAreaView,
 } from "react-native";
 import shirt1 from "../../assets/shirt1.png";
+import jeans1 from "../../assets/jeans1.png";
+import shoes1 from "../../assets/shoes1.png";
 
 export default function AlbumsScreen({ navigation }) {
+  const [selectedAlbum, setSelectedAlbum] = useState(null);
+  const windowSize = Dimensions.get("window").width;
+
   const data = [
-    { src: shirt1 },
-    { src: shirt1 },
-    { src: shirt1 },
-    { src: shirt1 },
-    { src: shirt1 },
-    { src: shirt1 },
-    { src: shirt1 },
+    {
+      name: "Shirts",
+      images: [
+        { src: shirt1 },
+        { src: shirt1 },
+        { src: shirt1 },
+        { src: shirt1 },
+        { src: shirt1 },
+        { src: shirt1 },
+        { src: shirt1 },
+      ],
+    },
+    {
+      name: "Jeans",
+      images: [
+        { src: jeans1 },
+        { src: jeans1 },
+        { src: jeans1 },
+        { src: jeans1 },
+        { src: jeans1 },
+        { src: jeans1 },
+        { src: jeans1 },
+      ],
+    },
+    {
+      name: "Shoes",
+      images: [
+        { src: shoes1 },
+        { src: shoes1 },
+        { src: shoes1 },
+        { src: shoes1 },
+        { src: shoes1 },
+        { src: shoes1 },
+        { src: shoes1 },
+      ],
+    },
   ];
+
+  const setAlbum = (albumName) => {
+    setSelectedAlbum(albumName);
+  };
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
@@ -29,19 +67,49 @@ export default function AlbumsScreen({ navigation }) {
         showsHorizontalScrollIndicator={false}
         data={data}
         renderItem={({ item }) => (
-          <Image
-            source={item.src}
-            style={{
-              width: 200,
-              height: 200,
-              borderWidth: 1,
-              borderColor: "blue",
-              resizeMode: "contain",
-              marginHorizontal: 5,
-            }}
-          />
+          <TouchableOpacity onPress={() => setAlbum(item.name)}>
+            <Image
+              //setting first image to be albumn cover
+              //could give user option later so im just marking it
+              source={item.images[0].src}
+              style={{
+                width: windowSize / 3,
+                height: windowSize / 3,
+                borderWidth: 1,
+                borderColor: "blue",
+                resizeMode: "contain",
+                marginHorizontal: 2,
+              }}
+            />
+          </TouchableOpacity>
         )}
       />
+      {selectedAlbum && (
+        <FlatList
+          ListHeaderComponent={
+            <Text style={{ fontSize: 30, textAlign: "center", color: "black" }}>
+              {selectedAlbum}
+            </Text>
+          }
+          data={data.find((item) => item.name === selectedAlbum).images.slice(1)}
+          horizontal={false}
+          numColumns={3}
+          renderItem={({ item }) => (
+            <Image
+              source={item.src}
+              style={{
+                marginVertical: 3,
+                width: windowSize / 3,
+                height: windowSize / 3,
+                borderWidth: 1,
+                borderColor: "blue",
+                resizeMode: "contain",
+                marginHorizontal: 1,
+              }}
+            />
+          )}
+        />
+      )}
     </SafeAreaView>
   );
 }
